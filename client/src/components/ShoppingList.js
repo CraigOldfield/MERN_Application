8075107c24ea5cd9c3e00e9d2a300f6b09c512f8
CraigOldfield <C_Oldfield@hotmail.com>
2019-03-17 15:57:05 +1100

    Initial commit of shoppingList component, this is the basic functionality of adding and deleting items without the database portion.

---
+++ client/src/components/ShoppingList.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import uuid from 'uuid';
+
+class ShoppingList extends Component {
+    state = {
+        items: [
+            { id: uuid(), name: 'Chocolate' },
+            { id: uuid(), name: 'Butter' },
+            { id: uuid(), name: 'Milk' },
+            { id: uuid(), name: 'Sugar' },
+        ]
+    }
+
+    render(){
+        const { items } = this.state;
+        return( 
+            <Container>
+                <Button 
+                    color="dark" 
+                    style={{ marginBottom: '2rem' }} 
+                    onClick={() => {
+                        const name = prompt('Enter Item');
+                        if(name){
+                            this.setState(state => ({
+                                items: [...state.items, { id: uuid(), name: name }]
+                            }));
+                        }
+                    }}
+                >
+                    Add Item
+                </Button>
+
+                <ListGroup>
+                    <TransitionGroup classNames="shopping-list">
+                        {items.map(({ id, name}) => (
+                            <CSSTransition key={id} timeout={500} classNames="fade">
+                                <ListGroupItem>
+                                    <Button 
+                                        classNames="remove-btn" 
+                                        color="danger" 
+                                        size="sm" 
+                                        onClick={() => {
+                                            this.setState(state => ({
+                                                items: state.items.filter(item => item.id !== id)
+                                            }));
+                                        }}>
+                                    &times;
+                                    </Button>
+                                    {name}
+                                </ListGroupItem>
+                            </CSSTransition>
+                        ))}
+                    </TransitionGroup>
+                </ListGroup>
+            </Container>
+        );
+    }
+}
+
+export default ShoppingList;
